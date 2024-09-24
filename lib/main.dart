be8e1215fecf4e6f7b2d6b8dd5a444308d783a1b
Bruno Maass <brunomaass@yahoo.com.br>
2024-09-24 10:53:15 -0400

    criado appBar personalizado

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gestao_de_estoque_2/pagina/home_page.dart';
 
 void main () {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: HomePage(),
   ));
 }
